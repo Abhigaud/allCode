483cd23fef23119123859a5441a635525edbac57
Abhigaud <abhishekgaud.annu@gmail.com>
2025-01-21 23:48:29 +0530

    9_loops assignment

---
+++ JavaFiles/9_loops/a1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class a1 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++){
+            System.out.println("hello");
+            i+=2;
+        }
+    }
+}
