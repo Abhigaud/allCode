5472dfb50fb9ed00629e51d05d59c3aadf05a0bc
Abhigaud <abhishekgaud.annu@gmail.com>
2025-01-22 21:08:01 +0530

    11_function start

--- JavaFiles/11_function/f1_file.java
+++ JavaFiles/11_function/f1_file.java
@@ -1,6 +1,10 @@
 
 public class f1_file {
+    public static void printHello(){
+        System.out.println("hello World");
+    }
     public static void main(String[] args) {
-        
+        printHello();
     }    
+
 }
