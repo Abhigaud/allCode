6d820a424b2f8deff096eb73c65c1b1772c3590b
Abhigaud <abhishekgaud.annu@gmail.com>
2025-03-08 18:13:27 +0530

    teachnook age & distance calculator

--- JavaFiles/14_arrays2/practice10/p1_leetcode33.java
+++ JavaFiles/14_arrays2/practice10/p1_leetcode33.java
@@ -28,8 +28,8 @@ public class p1_leetcode33 {
             return -1;
         }
         public static void main(String[] args) {
-                int nums[] ={40, 50, 10, 20, 30};
-                int target = 30;
+                int nums[] ={4,5,6,7,0,1,2};
+                int target = 3;
                 System.out.println(search(nums, target));
 
         }
