375a2373df82dbdfdc35c9d97d3a3b4b713c6532
Abhigaud <abhishekgaud.annu@gmail.com>
2025-03-19 23:49:09 +0530

    String continue

---
+++ JavaFiles/17_strings/f3_q2_shortestPath.java
@@ -0,0 +1,34 @@
+public class f3_q2_shortestPath {
+    public static float getShortestPath(String path) {
+        int x=0, y=0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // South
+            if (dir == 'S') {
+                y--;
+            }
+            // north
+            else if (dir == 'N') {
+                y++;
+            }
+            // West
+            else if (dir == 'W') {
+                x--;
+            }
+            // east
+            else{
+                x++;
+            }
+            
+        }
+
+        int x2 = x*x;
+        int y2 = y*y;
+        return (float)Math.sqrt(x2 + y2);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }    
+}
