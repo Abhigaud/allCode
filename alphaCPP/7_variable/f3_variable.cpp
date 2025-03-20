94790bc91380dc19ff217de1ef370c154cb1e1fc
Abhigaud <abhishekgaud.annu@gmail.com>
2025-03-20 23:28:25 +0530

    cpp 7

---
+++ alphaCPP/7_variable/f3_variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    // float and Double
+    float PI = 3.14159265359;
+    double PI2 = 3.14159265359;
+
+    cout<<setprecision(12)<<"PI = "<< PI << endl;
+    cout<<setprecision(12)<<"PI2 = "<< PI2 << endl;
+    return 0;
+}
---
+++ alphaCPP/7_variable/f4_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ;
+    int b;
+    cout<<"enter a: "<<endl;
+    cin >> a;
+    cout<<"enter b: "<<endl;
+    cin >> b;
+    int sum = a+b;
+    cout <<sum<<endl;
+}
