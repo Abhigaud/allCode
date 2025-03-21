2931742f1948fa108e0dc13f9d1cdd04a7c0a112
Abhigaud <abhishekgaud.annu@gmail.com>
2025-03-21 18:38:53 +0530

    C# start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        var pi = 3.14;
+        
+        Console.WriteLine("hello world");
+        Console.WriteLine(pi);
+    }
+}
+
---
+++ dotnet/program1.cs
@@ -0,0 +1,10 @@
+using System;
+class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine("hello world");
+    }
+}
+
+
