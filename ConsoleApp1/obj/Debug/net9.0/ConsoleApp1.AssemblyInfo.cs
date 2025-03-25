1173e741fa2f50aa5c5c0b0ee246a79bc245ec35
Abhigaud <abhishekgaud.annu@gmail.com>
2025-03-25 21:30:50 +0530

    teachnook 17 march

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1f843b98d27fc949546dfe5b3fb19c3f04f3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622cca7d06ba8ac07b251b2c22644c61e01a7cf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
