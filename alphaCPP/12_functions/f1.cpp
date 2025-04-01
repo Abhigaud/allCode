ca5576a55754a9bd99a2955bcc7f29d553162316
Abhigaud <abhishekgaud.annu@gmail.com>
2025-04-01 20:24:55 +0530

    function cpp

---
+++ alphaCPP/12_functions/f1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+void sayHello(){
+    cout << "Hello : )\n";
+}
+
+int sumAb(int a, int b){
+    int sum = a+b;
+    return sum;
+}
+
+int product (int a, int b){
+    return a*b;
+}
+
+bool OddEven(int a){
+    
+}
+
+int main(){
+    sayHello();
+    
+    int a = 5, b = 6;
+    int sum = sumAb(a, b);
+    cout << sum<<endl;
+    
+    product(a, b);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
