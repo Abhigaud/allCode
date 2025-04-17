03625312872f0d4d77c9d20cb7c9c8e481db7a54
Abhigaud <abhishekgaud.annu@gmail.com>
2025-04-17 20:40:40 +0530

    array 1

--- JavaFiles/13_arrays1/f6_pairsInArray.java
+++ JavaFiles/13_arrays1/f6_pairsInArray.java
@@ -10,6 +10,14 @@ public class f6_pairsInArray {
         }
     }
 
+    public static void pairsArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                System.out.print("[" + curr +", " + arr[j] +"]");
+            }
+        }
+    }
     
     public static void subarray(int numbers[]) {
         for (int i = 0; i < numbers.length; i++) {
@@ -25,11 +33,26 @@ public class f6_pairsInArray {
         }
     }
 
-    
+    public static void subArrayPrint(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr.length; j++){
+                int sum = 0;
+                for(int k = i; k <=j; k++){
+                    sum +=arr[k];
+                }
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
         int arr[] = {2, 4, 6, 8, 10};
 
+        // pairsArr(arr);
+        subArrayPrint(arr);
         // pairs(arr);
-        subarray(arr);
+        // subarray(arr);
     }
 }
