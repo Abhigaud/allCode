77776cde1acefc048238942c81ebdd6c532441ad
Abhigaud <abhishekgaud.annu@gmail.com>
2025-04-18 14:06:49 +0530

    array 1

--- JavaFiles/13_arrays1/f6_pairsInArray.java
+++ JavaFiles/13_arrays1/f6_pairsInArray.java
@@ -34,6 +34,7 @@ public class f6_pairsInArray {
     }
 
     public static void subArrayPrint(int arr[]){
+        int maxArrSum = Integer.MIN_VALUE ; 
         for(int i = 0; i < arr.length; i++){
             for(int j = i; j < arr.length; j++){
                 int sum = 0;
