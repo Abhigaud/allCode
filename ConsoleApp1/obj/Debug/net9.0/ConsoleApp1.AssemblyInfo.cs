0fff674b20f0fe9d57f0dfc6043087af94dc57fa
Abhigaud <abhishekgaud.annu@gmail.com>
2025-04-24 21:33:56 +0530

    java

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd415778546bb6b898d6a43dead783b4f53f6a16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b193e24d2f9e23d6d80e04c6969c9b532a3eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
